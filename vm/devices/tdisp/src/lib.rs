8b94d4ad6e8f9021c70cc9fd39e63e5c3f4493d0
mfrohlich <mfrohlich@microsoft.com>
2026-04-19 12:13:01 -0700

    tdisp: add DeviceTeardown unbind reason

--- vm/devices/tdisp/src/lib.rs
+++ vm/devices/tdisp/src/lib.rs
@@ -779,7 +779,9 @@ impl TdispGuestRequestInterface for TdispHostStateMachine {
         // if the guest says it is unbinding due to a host-related error), the reason is discarded and InvalidGuestUnbindReason
         // is recorded in the unbind history.
         let reason = match reason {
-            TdispGuestUnbindReason::Graceful => TdispUnbindReason::GuestInitiated(reason),
+            TdispGuestUnbindReason::Graceful | TdispGuestUnbindReason::DeviceTeardown => {
+                TdispUnbindReason::GuestInitiated(reason)
+            }
             _ => {
                 tracing::error!(
                     "Invalid guest unbind reason {} requested",
